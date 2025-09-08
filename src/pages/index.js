02be9dc8956172c9afbd0b44b17790d2a26d8c18
Varna Sri Raman <107885988+Varnasr@users.noreply.github.com>
2025-09-08 21:14:35 +0530

    Create index.js

---
+++ src/pages/index.js
@@ -0,0 +1,22 @@
+import AIToolsPage from './AIToolsPage.js';
+import AboutPage from './AboutPage.js';
+import CoursesPage from './CoursesPage.js';
+import DashboardPage from './DashboardPage.js';
+import FAQPage from './FAQPage.js';
+import GamesPage from './GamesPage.js';
+import HomePage from './HomePage.js';
+import LabsPage from './LabsPage.js';
+import ResourcesPage from './ResourcesPage.js';
+
+export {
+  AIToolsPage,
+  AboutPage,
+  CoursesPage,
+  DashboardPage,
+  FAQPage,
+  GamesPage,
+  HomePage,
+  LabsPage,
+  ResourcesPage,
+};
+
