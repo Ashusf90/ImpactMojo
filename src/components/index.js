763e4660ab1443ffcb0b8bebaeb9bc846fec5447
Varna Sri Raman <107885988+Varnasr@users.noreply.github.com>
2025-09-08 19:04:31 +0530

    Create index.js

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Logo } from './Logo.js';
+export { default as Navigation } from './Navigation.js';
+export { default as QuizResult } from './QuizResult.js';
+export { default as SimpleQuiz } from './SimpleQuiz.js';
+export { ImprovedFloatingActionButtons } from './floating-action-buttons.js';
+export { FeaturedContentSection, TestimonialsSection, PopularCoursesSection } from './homepage-components.js';
+export { LearningTracksSection } from './learning-tracks-component.js';
